4c4605121f395ff36cab55fdfd2522831df1fd93
ohsofoxy273 <ohsofoxy273@hotmail.com>
2014-02-26 21:29:09 -0600

    Modified erb_example.rb

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,31 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+my_array = %w[apples, bananas, grapes]
+my_hash = {vegetable: :carrot, starch: :spaghetti}
+class Person 
+	def initialize(name, email)
+		@name = name
+		@email = email
+	end
+
+	def name
+		@name
+	end
+
+	def email
+		@email
+	end
+end
+template = ERB.new "The value of x is: <%= x %>
+<% matt = Person.new('matt', 'matt@example.com') %>
+	My name is <%=matt.name %>
+	My email is <%= matt.email %>
+	These are some fruits:
+	<% my_array.each do |fruit|  %>
+		<%= fruit %>
+	<% end %>
+	And a vegetable:
+	<%= my_hash[:vegetable] %>
+"
 puts template.result(binding)
